3895e2d1fcefc813df680f3a503686d0b1e94821
Jonny <jonny788@live.co.uk>
2020-07-29 00:52:16 +0100

    restructured exsisting program to 2 functions and main

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import numpy as np
 import cv2
 import pyautogui
+import time
 
-def saveScreenShot():
-	# take screenshot using pyautogui 
-	image = pyautogui.screenshot() 
-   
-	# since the pyautogui takes as a  
-	# PIL(pillow) and in RGB we need to  
-	# convert it to numpy array and BGR  
-	# so we can write it to the disk 
-	image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) 
-	# writing it to the disk using opencv 
-	cv2.imwrite("image1.png", image)
 
 
 def findMatchButton():
+	print("finding findMatch button")
 	try:
 		pyautogui.click('images/findMatch.png')
 	except:
@@ -24,17 +15,23 @@ def findMatchButton():
 
 
 def findAcceptButton():
+	print("finding Accept button")
 	try:
 		pyautogui.click('images/accept.png')
+		return True
 	except:
 		print("ERROR:AcceptButton not found")
+		
+			
 
+searching = True
 
-
-findMatchButton()
-
-
-findAcceptButton()
+while searching == True:
+	
+	if findAcceptButton() == True:
+		break
+	else:
+		time.sleep(5)
 
 
 print("end program")
