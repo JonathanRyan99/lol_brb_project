9fde1a2e01bbe03689a7cd8117b72ee58a3d0d98
Jonny <jonny788@live.co.uk>
2020-07-29 19:32:38 +0100

    added tesseract to enviroment and confirmed its working, added read me file for setup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import pyautogui
 import time
-
+import pytesseract
 
 
 def findMatchButton():
@@ -24,7 +24,20 @@ def findAcceptButton():
 		
 			
 
-searching = True
+def imageToString():
+	img_cv = cv2.imread(r'test.png')
+	# By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
+	# we need to convert from BGR to RGB format/mode:
+	img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+	print(pytesseract.image_to_string(img_rgb))
+	
+
+
+
+imageToString()
+
+
+searching = False
 
 while searching == True:
 	
