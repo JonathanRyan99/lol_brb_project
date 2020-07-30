179f9e194a31fa60c2c4514e6cdc8adecfdad64b
Jonny <jonny788@live.co.uk>
2020-07-30 14:34:33 +0100

    didtn upload the previous times...

---
+++ README.txt
@@ -0,0 +1,26 @@
+libarys used in this:
+install the following to an enviroment
+
+python 3.8
+
+
+
+opencv
+numpy - comes with opencv when you pip install
+"pip install opencv-python"
+only standard libary used here
+
+
+
+pyautogui
+"pip install pyautogui"
+
+pytesseract 
+"pip install pytesseract"
+
+(a wrapper for python )
+
+this requires the isntallation of the google command line application tesseract
+
+https://pypi.org/project/pytesseract/
+
---
+++ search.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import pyautogui
+import time
+import pytesseract
+
+
+def findMatchButton():
+	print("finding findMatch button")
+	try:
+		pyautogui.click('images/findMatch.png')
+	except:
+		print("ERROR:MatchButton not found")
+
+
+
+def findAcceptButton():
+	print("finding Accept button")
+	try:
+		pyautogui.click('images/accept.png')
+		return True
+	except:
+		print("ERROR:AcceptButton not found")
+		
+			
+
+
+searching = True
+
+while searching == True:
+	
+	if findAcceptButton() == True:
+		break
+	else:
+		time.sleep(5)
+
+
+print("end program")
+
+
+
